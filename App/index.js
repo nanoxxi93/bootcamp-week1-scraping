b2c80e1ac95821daa70835ede3bbea0946162f57
nanoxxi93 <hernandoarima@gmail.com>
2021-03-16 00:10:23 -0500

    Week 1 Day 1

--- App/index.js
+++ App/index.js
@@ -11,7 +11,7 @@ btnscrap.addEventListener('click', async () => {
   }
 })
 
-const scrapingProfile = () => {
+const scrapingProfile = async () => {
   const wait = function (milliseconds) {
     return new Promise(function (resolve) {
       setTimeout(function () {
@@ -21,16 +21,84 @@ const scrapingProfile = () => {
   };
 
 
-  const elementNameProfile = document.querySelector("div.ph5.pb5 > div.display-flex.mt2 ul li")
-  const elementNameTitle = document.querySelector("div.ph5.pb5 > div.display-flex.mt2 h2")
-  const name = elementNameProfile ? elementNameProfile.innerText : '';
-  const title = elementNameTitle ? elementNameTitle.innerText : '';
-  wait(2000)
-  const elementMoreResume = document.getElementById('line-clamp-show-more-button')
+  const autoscrollToElement = async function (cssSelector) {
+    var exists = document.querySelector(cssSelector) != null ? true : false;
+    console.log(exists);
+    while (exists) {
+      let maxScrollTop = document.body.clientHeight - window.innerHeight;
+      let elementScrollTop = document.querySelector(cssSelector).offsetHeight + window.innerHeight;
+      let currentScrollTop = window.scrollY;
+
+      console.log(maxScrollTop);
+      console.log(currentScrollTop);
+
+      if (maxScrollTop == currentScrollTop || elementScrollTop <= currentScrollTop) {
+        break;
+      }
+
+      await wait(150)
+      window.scrollBy(0, window.innerHeight/5);
+    }
+  }
+
+  const summaryProfile = document.querySelector('section.pv-top-card ul.pv-top-card--list').parentElement;
+  const name = summaryProfile.querySelector('ul.pv-top-card--list li').innerText;
+  const title = summaryProfile.querySelector('h2').innerText;
+  const location = summaryProfile.querySelectorAll('ul.pv-top-card--list')[1]?.querySelector('li')?.innerText;
+  wait(2000);
+  const elementMoreResume = document.querySelector('#line-clamp-show-more-button');
   if (elementMoreResume) elementMoreResume.click();
-  const elementResume = document.querySelector('section.pv-about-section > p')
-  const resume = elementResume ? elementNameTitle.innerText : '';
+  const elementResume = document.querySelector('section.pv-about-section p.pv-about__summary-text');
+  const resume = elementResume.innerText;
 
+  const profileDetail = document.querySelector('div.profile-detail');
+  await autoscrollToElement('div.profile-detail');
 
-  console.log({ name, title, resume })
+  let experienceDOM = document.querySelector('section.experience-section')
+  let experienceList = experienceDOM.querySelectorAll('section');
+  let experience = []
+  experienceList.forEach(x => {
+    if (x.querySelector('ul') != null) {
+      let companyDetails = x.querySelector('div.pv-entity__company-details');
+      x.querySelectorAll('li.pv-entity__position-group-role-item').forEach(y => {
+        y.querySelector('button.inline-show-more-text__button')?.click();
+        let element = y.querySelector('h3').parentElement;
+        experience.push({
+          "job": element.querySelector('h3')?.querySelector('span:not(.visually-hidden)')?.innerText,
+          "company": companyDetails.querySelector('h3')?.querySelector('span:not(.visually-hidden)')?.innerText,
+          "date": element.querySelector('h4.pv-entity__date-range')?.querySelector('span:not(.visually-hidden)')?.innerText,
+          "location": element.querySelector('h4.pv-entity__location')?.querySelector('span:not(.visually-hidden)')?.innerText,
+          "description": y.querySelector('div.pv-entity__extra-details')?.querySelector('p.pv-entity__description')?.innerText
+        });
+      });
+    }
+    else {
+      x.querySelector('button.inline-show-more-text__button')?.click();
+      let element = x.querySelector('h3').parentElement;
+      experience.push({
+        "job": element.querySelector('h3')?.innerText,
+        "company": element.querySelector('p.pv-entity__secondary-title')?.firstChild.wholeText.trim(),
+        "date": element.querySelector('h4.pv-entity__date-range')?.querySelector('span:not(.visually-hidden)')?.innerText,
+        "location": element.querySelector('h4.pv-entity__location')?.querySelector('span:not(.visually-hidden)')?.innerText,
+        "description": x.querySelector('div.pv-entity__extra-details')?.querySelector('p.pv-entity__description')?.innerText
+      });
+    }
+  });
+
+  let educationDOM = document.querySelector('section.education-section');
+  let educationList = educationDOM.querySelectorAll('.pv-education-entity');
+  let education = []
+  educationList.forEach(x => {
+    let element = x.querySelector('div.pv-entity__summary-info');
+    let degreeInfo = element.querySelector('div.pv-entity__degree-info');
+    education.push({
+      "schoolName": degreeInfo.querySelector('h3.pv-entity__school-name')?.innerText,
+      "degree": degreeInfo.querySelectorAll('p.pv-entity__secondary-title')[0]?.querySelector('span:not(.visually-hidden)')?.innerText,
+      "study": degreeInfo.querySelectorAll('p.pv-entity__secondary-title')[1]?.querySelector('span:not(.visually-hidden)')?.innerText,
+      "date": element.querySelector('p.pv-entity__dates')?.querySelector('span:not(.visually-hidden)')?.innerText,
+      "description": x.querySelector('div.pv-entity__extra-details')?.querySelector('p.pv-entity__description')?.innerText
+    });
+  });
+
+  console.log({ name, title, location, resume, experience, education });
 }
